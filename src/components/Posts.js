055f7b28c8d53fb38a600f81d2f6de3428f75fe8
John-Kimani <kimwaweru100@gmail.com>
2022-08-31 12:48:41 +0300

    Set up to display posts from server and on oninnit a loader is displayed

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,38 +1,21 @@
+import { useState } from "react";
+import { Spinner } from "react-bootstrap";
+
 export default function Posts() {
-    const posts = [
-      {
-        id: 1,
-        text: 'Hello, world!',
-        timestamp: 'a minute ago',
-        author: {
-          username: 'susan',
-        },
-      },
-      {
-        id: 2,
-        text: 'Second post',
-        timestamp: 'an hour ago',
-        author: {
-          username: 'john',
-        },
-      },
-    ];
+
+  const [posts , setPosts] = useState();
+
+  // TODO: add a side effect function to request posts here
   
     return (
       <>
-        {posts.length === 0 ?
-          <p>There are no blog posts.</p>
-        :
-          posts.map(post => {
-            return (
-              <p key={post.id}>
-                <b>{post.author.username}</b> &mdash; {post.timestamp}
-                <br />
-                {post.text}
-              </p>
-            );
-          })
-        }
+      {posts === undefined ?
+      <Spinner animation='border' />
+      :
+      <>
+      ...
+      </>  
+      }
       </>
     );
   }
