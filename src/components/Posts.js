7bac4c71566e1b6c53b0b4505ad35690dce3d9f8
John-Kimani <kimwaweru100@gmail.com>
2022-08-31 16:48:34 +0300

    Creating a side effect function

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,26 @@
 import { useState } from "react";
 import { Spinner } from "react-bootstrap";
 
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+
 export default function Posts() {
 
   const [posts , setPosts] = useState();
 
   // TODO: add a side effect function to request posts here
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(BASE_API_URL + 'api/feed');
+      if (response.ok) {
+        const results = await response.json();
+        setPosts(results.data);
+      }else {
+        setPosts(null);
+      }
+    });
+  }, []);
+  
   
     return (
       <>
@@ -13,7 +28,11 @@ export default function Posts() {
       <Spinner animation='border' />
       :
       <>
-      ...
+        {posts === null ?
+        <p>Could not retrive blog posts.</p> :
+        <>
+
+        </>}
       </>  
       }
       </>
