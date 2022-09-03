c53226ae4bae8de86920a3bf9eb6d2b3459ad070
John-Kimani <kimwaweru100@gmail.com>
2022-09-03 07:50:19 +0300

    Setup TimeAgo default function

--- src/components/TimeAgo.js
+++ src/components/TimeAgo.js
@@ -38,4 +38,19 @@ function getTimeAgo(date) {
 
 export default function TimeAgo({isoDate}){
     //TimeAgo
+    const date = new Date(Date.parse(isoDate));
+    const [time, unit, interval] = getTimeAgo(date);
+    const [, setUpdate] = useState(0);
+
+    useEffect(() => {
+        const timerId = setInterval(
+            ()=> setUpdate(update => update + 1),
+            interval * 1000
+        );
+        return () => clearInterval(timerId);
+    }, [interval]);
+
+    return (
+        <span title={date.toString()}>{rtf.format(time, unit)}</span>
+    )
 }
